6449467e25394c5cf4ffca47275b43d925a25419
aravindgitsk <aravind.mtp@gmail.com>
2021-02-27 14:02:34 +0530

    2nd try

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -1,8 +1,12 @@
 package org.phone;
 
 public class ExternalStorage {
-	public void size() {
+	public static void size() {
 		System.out.println("size - 256gb");
+		System.out.println("singam 1");
 
+	}
+	public static void main(String[] args) {
+		size();
 	}
 }
